d9865406042bd455dceb300d586f753f17634aab
MOAAS <38762849+MOAAS@users.noreply.github.com>
2019-03-27 22:06:44 +0000

    + pequenos fixes no modelo relacional. algumas classes feitas em SQL

--- criar.sql
+++ criar.sql
@@ -4,7 +4,7 @@ PRAGMA foreign_keys = OFF;
 drop table if exists Person;
 drop table if exists Employee;
 drop table if exists Passenger;
-drop table if exists Boss;
+drop table if exists IsBoss;
 drop table if exists Country;
 drop table if exists City;
 drop table if exists Airport;
@@ -29,6 +29,73 @@ drop table if exists LuggageCounter;
 PRAGMA foreign_keys = ON;
 
 create table Person (
-    name TEXT PRIMARY KEY, 
-    country TEXT
+    NIF TEXT PRIMARY KEY,
+    personName TEXT,
+    BirthDate TEXT,
+    PhoneNumber TEXT,
+    country INTEGER REFERENCES Country(CountryID)
 );
+
+create table Employee (
+    PersonID TEXT REFERENCES Person(NIF),
+    Salary INTEGER,
+    Workplace REFERENCES Workplace(WorkplaceID),
+    PRIMARY KEY (PersonID)
+);
+
+create table Passenger (
+    PersonID TEXT REFERENCES Person(NIF),
+    IDnumber TEXT,
+    PRIMARY KEY (PersonID)
+);
+
+create table IsBoss (
+    BossID INTEGER REFERENCES Employee(Person),
+    BossedID INTEGER REFERENCES Employee(Person),
+    CHECK (BossID <> BossedID)
+);
+
+create table Country (
+    CountryID INTEGER PRIMARY KEY,
+    CountryName TEXT
+);
+
+create table City ( 
+    CityID INTEGER,
+    CountryID INTEGER,
+    CityName TEXT,
+    PRIMARY KEY (CityID),
+    FOREIGN KEY (CountryID) REFERENCES Country(CountryID)
+);
+
+create table Airport (
+    AirportID INTEGER,
+    CityID INTEGER,
+    AirportName TEXT,
+    AirportCode TEXT,
+    PRIMARY KEY (AirportID),
+    FOREIGN KEY (CityID) REFERENCES City(CityID)
+);
+
+-- create table Trip
+-- create table Ticket
+-- create table Luggage
+-- create table LuggageDropOff
+-- create table Airplane
+-- create table Airline
+-- create table AirplaneModel
+-- create table Class
+-- create table ClassRows
+
+create table Workplace (
+    WorkplaceID INTEGER PRIMARY KEY,
+    WorkplaceName TEXT
+);
+
+-- create table Gate
+-- create table Strip
+-- create table Desk
+-- create table CheckInDesk
+-- create table HelpDesk
+-- create table HasDesk
+-- create table LuggageCounter
