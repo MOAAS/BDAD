7cc2683e45e633a18cf49708cd7bb63c77093f5c
MOAAS <miguel.moas@gmail.com>
2019-05-26 00:36:49 +0100

    fixed critical bug

--- Entrega 3/int2.sql
+++ Entrega 3/int2.sql
@@ -4,6 +4,6 @@
 .headers on
 .nullvalue NULL
 
-SELECT CityName, count(*) AS CountFROM (
+SELECT CityName, count(*) AS Count FROM (
     Departure NATURAL JOIN Trip NATURAL JOIN Airport NATURAL JOIN City
 ) GROUP BY CityID ORDER BY Count DESC LIMIT 5;
