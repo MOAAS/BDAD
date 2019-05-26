40f9950d1a7092f1ffd87672a72d19907cbf5de7
MOAAS <miguel.moas@gmail.com>
2019-05-26 22:45:45 +0100

    mudanças finais

--- Entrega 3/testar.sql
+++ Entrega 3/testar.sql
@@ -1,22 +1,8 @@
 .read criar.sql
-
-.read gatilho1_adiciona.sql
-.read gatilho2_adiciona.sql
-.read gatilho3_adiciona.sql
-
 .read povoar.sql
 
-.read gatilho1_verifica.sql
-.read gatilho2_verifica.sql
-.read gatilho3_verifica.sql
-
-.read gatilho1_remove.sql
-.read gatilho2_remove.sql
-.read gatilho3_remove.sql
-
 .read int1.sql
-.read int2min.sql
-.read int2max.sql 
+.read int2.sql
 .read int3.sql
 .read int4.sql
 .read int5.sql
@@ -25,3 +11,15 @@
 .read int8.sql
 .read int9.sql
 .read int10.sql
+
+.read gatilho1_adiciona.sql
+.read gatilho2_adiciona.sql
+.read gatilho3_adiciona.sql
+
+.read gatilho1_verifica.sql
+.read gatilho2_verifica.sql
+.read gatilho3_verifica.sql
+
+.read gatilho1_remove.sql
+.read gatilho2_remove.sql
+.read gatilho3_remove.sql
