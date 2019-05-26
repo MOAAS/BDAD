e25af682c7136731ccb0d3db1599110c2607a838
MOAAS <miguel.moas@gmail.com>
2019-05-26 12:35:51 +0100

    adicionado query de luggage belts :100:

---
+++ Entrega 3/int9.sql
@@ -0,0 +1,10 @@
+-- Lista de arrivals com respetivos luggagebelts disponiveis
+
+SELECT Arr1.TripID, LuggageBelt.BeltNum AS AvailableBeltNum, Arr1.DropoffDate, Arr1.DropoffTime FROM Arrival AS Arr1, LuggageBelt WHERE NOT EXISTS (
+    SELECT Arr2.BeltID FROM Arrival As Arr2 WHERE (
+        Arr2.BeltID = LuggageBelt.WorkplaceID AND 
+        Arr2.DropoffDate = Arr1.DropoffDate AND 
+        Arr2.DropoffTime = Arr1.DropoffTime
+    )
+)
+ORDER BY TripID, LuggageBelt.BeltNum;
